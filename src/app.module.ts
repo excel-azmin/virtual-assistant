4b3ce5ec6d374c8192f430cf241cab73e6daca77
Shaid Azmin <excel.azmin@gmail.com>
2024-11-04 12:07:04 +0600

    update env configuraiton

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,12 +7,17 @@ import { UsersInfoModule } from './users-info/users-info.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb://root:mongorootPass2023@202.91.42.198:27017/virtualAssistantDB',
-      {
-        authSource: 'admin',
-      },
-    ),
+    ConfigModule.forRoot({
+      isGlobal: true, // This makes ConfigModule available globally
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+        authSource: configService.get<string>('MONGO_AUTH_SOURCE'),
+      }),
+      inject: [ConfigService],
+    }),
     UsersInfoModule,
   ],
   controllers: [AppController],
